b4d65ff86f00653ce57368f8dcc4fa6d4749a2f8
nykb <koranteng@smsgh.com>
2018-05-13 18:29:29 +0100

    sdsdsk

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
-use App\Models\Product;
+
 class ProductCategoryController extends Controller
 {
 
@@ -51,6 +51,8 @@ class ProductCategoryController extends Controller
 
 
     }
+
+
 
 }
 

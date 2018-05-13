ffde5ff6df1b429e85744c0d4ddc25528768cb36
nykb <koranteng@smsgh.com>
2018-05-13 14:22:34 +0100

    new changes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'posystemAPI'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+   // 'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://posystemapi.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
